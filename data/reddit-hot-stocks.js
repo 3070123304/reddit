4cb11560b628fd84f012184becda45e3ae841451
3070123304 <284909501+3070123304@users.noreply.github.com>
2026-06-10 21:33:25 +0000

    Update Reddit hot stocks

--- data/reddit-hot-stocks.js
+++ data/reddit-hot-stocks.js
@@ -1,5 +1,5 @@
 window.redditHotStocksData = {
-  "generatedAt": "2026-06-10T12:45:43.866Z",
+  "generatedAt": "2026-06-10T21:33:24.370Z",
   "window": "last_60_minutes",
   "source": "reddit",
   "subreddits": [
@@ -21,216 +21,224 @@ window.redditHotStocksData = {
     ],
     "items": [
       {
-        "title": "Philip Morris: IQOS And ZYN Offset Combustibles Volume Decline",
+        "title": "Core & Main's Drop Doesn't Offer An Entry Point",
         "source": "Latest Articles",
         "summary": "",
-        "publishedAt": "2026-06-10T12:43:59.000Z",
+        "publishedAt": "2026-06-10T21:26:11.000Z",
         "tickers": [],
-        "url": "https://seekingalpha.com/article/4913694-philip-morris-iqos-and-zyn-offset-combustibles-volume-decline?source=feed_all_articles"
+        "url": "https://seekingalpha.com/article/4913957-core-and-main-drop-doesnt-offer-an-entry-point?source=feed_all_articles"
       },
       {
-        "title": "Stock index futures pare losses as CPI comes in line; eyes on Middle East conflict",
+        "title": "Xtract One Technologies GAAP EPS of -C$0.001, revenue of C$10.26M",
         "source": "All News",
         "summary": "",
-        "publishedAt": "2026-06-10T12:43:55.000Z",
+        "publishedAt": "2026-06-10T21:24:25.000Z",
         "tickers": [],
-        "url": "https://seekingalpha.com/news/4602056-sp500-nasdaq-dow-jones-outlook-stock-market?utm_source=feed_news_all&utm_medium=referral&feed_item_type=news"
+        "url": "https://seekingalpha.com/news/4602416-xtract-one-technologies-gaap-eps-of-c0001-revenue-of-c1026m?utm_source=feed_news_all&utm_medium=referral&feed_item_type=news"
       },
       {
-        "title": "KLA Corp., Lam Research, Applied Materials, ASML in focus as Cantor ups price targets",
+        "title": "Iran: Sleepwalking Into A Crisis",
+        "source": "Latest Articles",
+        "summary": "",
+        "publishedAt": "2026-06-10T21:23:10.000Z",
+        "tickers": [],
+        "url": "https://seekingalpha.com/article/4913956-iran-sleepwalking-into-crisis?source=feed_all_articles"
+      },
+      {
+        "title": "Xbox to reboot with layoffs, budget cuts -- Bloomberg",
         "source": "All News",
         "summary": "",
-        "publishedAt": "2026-06-10T12:43:10.000Z",
+        "publishedAt": "2026-06-10T21:22:55.000Z",
         "tickers": [],
-        "url": "https://seekingalpha.com/news/4602148-kla-corp-lam-research-applied-materials-asml-in-focus-as-cantor-ups-price-targets?utm_source=feed_news_all&utm_medium=referral&feed_item_type=news"
+        "url": "https://seekingalpha.com/news/4602407-xbox-to-reboot-with-layoffs-budget-cuts---bloomberg?utm_source=feed_news_all&utm_medium=referral&feed_item_type=news"
       },
       {
-        "title": "Pennon Group Plc (PEGRY) Q4 2026 Earnings Call Transcript",
-        "source": "Latest Articles",
-        "summary": "",
-        "publishedAt": "2026-06-10T12:42:13.000Z",
-        "tickers": [],
-        "url": "https://seekingalpha.com/article/4913707-pennon-group-plc-pegry-q4-2026-earnings-call-transcript?source=feed_all_articles"
-      },
-      {
-        "title": "Heidelberger Druckmaschinen Aktiengesellschaft (HBGRY) Q4 2026 Earnings Call Transcript",
-        "source": "Latest Articles",
-        "summary": "",
-        "publishedAt": "2026-06-10T12:42:09.000Z",
-        "tickers": [],
-        "url": "https://seekingalpha.com/article/4913651-heidelberger-druckmaschinen-aktiengesellschaft-hbgry-q4-2026-earnings-call-transcript?source=feed_all_articles"
-      },
-      {
-        "title": "Taiwan tests U.S.-made missiles on China-facing coast in signal to Beijing",
+        "title": "Haivision Systems GAAP EPS of -C$0.07, revenue of C$32.54M",
         "source": "All News",
         "summary": "",
-        "publishedAt": "2026-06-10T12:41:24.000Z",
+        "publishedAt": "2026-06-10T21:21:24.000Z",
         "tickers": [],
-        "url": "https://seekingalpha.com/news/4602142-taiwan-tests-u-s-made-missiles-on-china-facing-coast-in-signal-to-beijing?utm_source=feed_news_all&utm_medium=referral&feed_item_type=news"
+        "url": "https://seekingalpha.com/news/4602415-haivision-systems-gaap-eps-of-c007-revenue-of-c3254m?utm_source=feed_news_all&utm_medium=referral&feed_item_type=news"
       },
       {
-        "title": "SecureTech appoints Anthony Vang as CFO",
+        "title": "AVAX One to implement 1-for-12 reverse stock split",
         "source": "All News",
         "summary": "",
-        "publishedAt": "2026-06-10T12:41:09.000Z",
+        "publishedAt": "2026-06-10T21:21:06.000Z",
         "tickers": [],
-        "url": "https://seekingalpha.com/news/4602147-securetech-appoints-anthony-vang-as-cfo?utm_source=feed_news_all&utm_medium=referral&feed_item_type=news"
+        "url": "https://seekingalpha.com/news/4602414-avax-one-to-implement-1-for-12-reverse-stock-split?utm_source=feed_news_all&utm_medium=referral&feed_item_type=news"
       },
       {
-        "title": "Royal Bank of Canada launches new NCIB to repurchase up to 45M common shares",
+        "title": "Teradyne wins $139.9M U.S. Air Force contract for test station kits update",
         "source": "All News",
         "summary": "",
-        "publishedAt": "2026-06-10T12:40:04.000Z",
+        "publishedAt": "2026-06-10T21:17:11.000Z",
         "tickers": [],
-        "url": "https://seekingalpha.com/news/4602149-royal-bank-of-canada-launches-new-ncib-to-repurchase-up-to-45m-common-shares?utm_source=feed_news_all&utm_medium=referral&feed_item_type=news"
+        "url": "https://seekingalpha.com/news/4602413-teradyne-wins-1399m-us-air-force-contract-for-test-station-kits-update?utm_source=feed_news_all&utm_medium=referral&feed_item_type=news"
       },
       {
-        "title": "Nowcast Data Suggest U.S. Growth Is Accelerating In Q2",
-        "source": "Latest Articles",
-        "summary": "",
-        "publishedAt": "2026-06-10T12:40:00.000Z",
-        "tickers": [],
-        "url": "https://seekingalpha.com/article/4913709-nowcast-data-suggest-us-growth-is-accelerating-in-q2?source=feed_all_articles"
-      },
-      {
-        "title": "CPI read: Coffee prices up double digits again in the U.S.",
+        "title": "BancFirst agrees to acquire SpiritBank",
         "source": "All News",
         "summary": "",
-        "publishedAt": "2026-06-10T12:39:52.000Z",
+        "publishedAt": "2026-06-10T21:16:42.000Z",
         "tickers": [],
-        "url": "https://seekingalpha.com/news/4602151-cpi-read-coffee-prices-up-double-digits-again-in-the-u-s?utm_source=feed_news_all&utm_medium=referral&feed_item_type=news"
+        "url": "https://seekingalpha.com/news/4602411-bancfirst-agrees-to-acquire-spiritbank?utm_source=feed_news_all&utm_medium=referral&feed_item_type=news"
       },
       {
-        "title": "UiPath: Agentic AI Overhang Overshadows 71% Incremental Profit Margins",
+        "title": "AMD: Party's Over -- This Chart Killed AI (Rating Downgrade)",
         "source": "Latest Articles",
         "summary": "",
-        "publishedAt": "2026-06-10T12:38:35.000Z",
-        "tickers": [],
-        "url": "https://seekingalpha.com/article/4913691-uipath-agentic-ai-overhang-overshadows-71-percent-incremental-profit-margins?source=feed_all_articles"
-      },
-      {
-        "title": "Fold eliminates $20M of secured debt, raises $25M in unrestricted cash to fund growth",
-        "source": "All News",
-        "summary": "",
-        "publishedAt": "2026-06-10T12:37:15.000Z",
-        "tickers": [],
-        "url": "https://seekingalpha.com/news/4602145-fold-eliminates-20m-of-secured-debt-raises-25m-in-unrestricted-cash-to-fund-growth?utm_source=feed_news_all&utm_medium=referral&feed_item_type=news"
-      },
-      {
-        "title": "Blackstone: The Market Is Selling Over A Problem That Doesn't Exist",
-        "source": "Latest Articles",
-        "summary": "",
-        "publishedAt": "2026-06-10T12:37:15.000Z",
-        "tickers": [],
-        "url": "https://seekingalpha.com/article/4913690-blackstone-market-selling-over-problem-that-doesnt-exist?source=feed_all_articles"
-      },
-      {
-        "title": "Casella Waste Systems, Inc. (CWST) Presents at Waste360/ Stifel Investor Summit - Slideshow",
-        "source": "Latest Articles",
-        "summary": "",
-        "publishedAt": "2026-06-10T12:31:46.000Z",
-        "tickers": [],
-        "url": "https://seekingalpha.com/article/4913712-casella-waste-systems-inc-cwst-presents-at-waste360-stifel-investor-summit-slideshow?source=feed_all_articles"
-      },
-      {
-        "title": "AMD's AI Story Is No Longer Speculative, It's Funded",
-        "source": "Latest Articles",
-        "summary": "",
-        "publishedAt": "2026-06-10T12:30:40.000Z",
+        "publishedAt": "2026-06-10T21:16:42.000Z",
         "tickers": [
           "AMD"
         ],
-        "url": "https://seekingalpha.com/article/4913688-amd-ai-story-is-no-longer-speculative-its-funded?source=feed_all_articles"
+        "url": "https://seekingalpha.com/article/4913954-amd-partys-over-this-chart-killed-ai-rating-downgrade?source=feed_all_articles"
       },
       {
-        "title": "Chewy, Inc. 2027 Q1 - Results - Earnings Call Presentation",
-        "source": "Latest Articles",
+        "title": "Navitas leads daily decline as semiconductor stocks suffer sell-off",
+        "source": "All News",
         "summary": "",
-        "publishedAt": "2026-06-10T12:30:38.000Z",
+        "publishedAt": "2026-06-10T21:15:26.000Z",
         "tickers": [],
-        "url": "https://seekingalpha.com/article/4913711-chewy-inc-2027-q1-results-earnings-call-presentation?source=feed_all_articles"
+        "url": "https://seekingalpha.com/news/4602390-navitas-leads-daily-decline-as-semiconductor-stocks-suffer-sell-off?utm_source=feed_news_all&utm_medium=referral&feed_item_type=news"
       },
       {
-        "title": "10x Genomics: Temporary Pain, Long-Term Gain As Atera Rollout Approaches",
+        "title": "Ares Management Corporation (ARES) Presents at Morgan Stanley US Financials Conference 2026 Transcript",
         "source": "Latest Articles",
         "summary": "",
-        "publishedAt": "2026-06-10T12:24:38.000Z",
+        "publishedAt": "2026-06-10T21:02:10.000Z",
         "tickers": [],
-        "url": "https://seekingalpha.com/article/4913687-10x-genomics-temporary-pain-long-term-gain-as-atera-rollout-approaches?source=feed_all_articles"
+        "url": "https://seekingalpha.com/article/4913901-ares-management-corporation-ares-presents-at-morgan-stanley-us-financials-conference-2026?source=feed_all_articles"
       },
       {
-        "title": "Teradyne's Rebound Leaves Little Room For Error",
+        "title": "SpaceX's Trillion&#x2011;Dollar Moonshot: 3 Aerospace And Defense Stocks Delivering 50%+ EPS Growth",
         "source": "Latest Articles",
         "summary": "",
-        "publishedAt": "2026-06-10T12:23:55.000Z",
+        "publishedAt": "2026-06-10T21:00:17.000Z",
         "tickers": [],
-        "url": "https://seekingalpha.com/article/4913686-teradynes-rebound-leaves-little-room-for-error?source=feed_all_articles"
+        "url": "https://seekingalpha.com/article/4913914-spacex-trillion-dollar-moonshot-3-aerospace-defense-stocks-delivering-50-percent-plus-eps-growth?source=feed_all_articles"
       },
       {
-        "title": "Invesco High Yield Municipal Fund Q1 2026 Commentary",
+        "title": "PLTY: This Isn't The Right Time For This High Risk Income Bet On Palantir",
         "source": "Latest Articles",
         "summary": "",
-        "publishedAt": "2026-06-10T12:14:00.000Z",
+        "publishedAt": "2026-06-10T20:59:19.000Z",
         "tickers": [],
-        "url": "https://seekingalpha.com/article/4913683-invesco-high-yield-municipal-fund-q1-2026-commentary?source=feed_all_articles"
+        "url": "https://seekingalpha.com/article/4913950-plty-this-isnt-the-right-time-for-this-high-risk-income-bet-on-palantir?source=feed_all_articles"
       },
       {
-        "title": "Credo Technology: The $3,000-Per-GPU Memory Arbitrage Demands A Strong Buy (Rating Upgrade)",
+        "title": "VIOV: Still Going Strong Amidst Market Volatility",
         "source": "Latest Articles",
         "summary": "",
-        "publishedAt": "2026-06-10T12:11:46.000Z",
+        "publishedAt": "2026-06-10T20:54:45.000Z",
         "tickers": [],
-        "url": "https://seekingalpha.com/article/4913681-credo-technology-the-3000-per-gpu-memory-arbitrage-demands-a-strong-buy-rating-upgrade?source=feed_all_articles"
+        "url": "https://seekingalpha.com/article/4913949-viov-still-going-strong-amidst-market-volatility?source=feed_all_articles"
+      },
+      {
+        "title": "BSTZ: How To Buy Anthropic With A Discount",
+        "source": "Latest Articles",
+        "summary": "",
+        "publishedAt": "2026-06-10T20:52:35.000Z",
+        "tickers": [],
+        "url": "https://seekingalpha.com/article/4913948-bstz-how-to-buy-anthropic-with-a-discount?source=feed_all_articles"
+      },
+      {
+        "title": "The North West Company Inc. (NWC:CA) Shareholder/Analyst Call Prepared Remarks Transcript",
+        "source": "Latest Articles",
+        "summary": "",
+        "publishedAt": "2026-06-10T20:52:14.000Z",
+        "tickers": [],
+        "url": "https://seekingalpha.com/article/4913908-the-north-west-company-inc-nwc-ca-shareholder-analyst-call-prepared-remarks-transcript?source=feed_all_articles"
+      },
+      {
+        "title": "Coty Inc. (COTY) Presents at 16th Annual East Coast IDEAS Conference Transcript",
+        "source": "Latest Articles",
+        "summary": "",
+        "publishedAt": "2026-06-10T20:52:12.000Z",
+        "tickers": [],
+        "url": "https://seekingalpha.com/article/4913858-coty-inc-coty-presents-at-16th-annual-east-coast-ideas-conference-transcript?source=feed_all_articles"
+      },
+      {
+        "title": "CACI International Inc (CACI) Presents at 16th Annual Wells Fargo Industrials & Materials Conference Transcript",
+        "source": "Latest Articles",
+        "summary": "",
+        "publishedAt": "2026-06-10T20:52:10.000Z",
+        "tickers": [],
+        "url": "https://seekingalpha.com/article/4913947-caci-international-inc-caci-presents-at-16th-annual-wells-fargo-industrials-and-materials?source=feed_all_articles"
+      },
+      {
+        "title": "Microsoft: OpenAI Exposure Manageable And Palantir Competition Contained",
+        "source": "Latest Articles",
+        "summary": "",
+        "publishedAt": "2026-06-10T20:52:06.000Z",
+        "tickers": [],
+        "url": "https://seekingalpha.com/article/4913945-microsoft-openai-exposure-manageable-and-palantir-competition-contained?source=feed_all_articles"
+      },
+      {
+        "title": "IREN's Plans For Hyper-Growth Are Likely To Significantly Disappoint",
+        "source": "Latest Articles",
+        "summary": "",
+        "publishedAt": "2026-06-10T20:50:42.000Z",
+        "tickers": [],
+        "url": "https://seekingalpha.com/article/4913943-iren-s-plans-for-hyper-growth-are-likely-to-significantly-disappoint?source=feed_all_articles"
       }
     ],
     "popularItems": [
       {
-        "title": "Broadcom: AI Red Flags Market Can't Ignore",
+        "title": "Sneak Peak Into The New Quant Growth & Income Portfolio: An Energy Giant Built For Volatility",
         "source": "Most Popular",
         "summary": "",
-        "publishedAt": "2026-06-10T09:32:22.000Z",
+        "publishedAt": "2026-06-10T13:25:00.000Z",
         "tickers": [],
-        "url": "https://seekingalpha.com/article/4913640-broadcom-ai-red-flags-market-cant-ignore?source=feed"
+        "url": "https://seekingalpha.com/article/4913739-inside-the-new-quant-portfolio-the-1-energy-giant-built-for-volatility?source=feed"
       },
       {
-        "title": "Micron Technology: The Widening Infrastructure Thesis",
+        "title": "Nasdaq Quant Leaders: 5 Top Stocks Averaging 272% Forward EPS Growth",
         "source": "Most Popular",
         "summary": "",
-        "publishedAt": "2026-06-10T09:25:09.000Z",
+        "publishedAt": "2026-06-10T13:00:00.000Z",
         "tickers": [],
-        "url": "https://seekingalpha.com/article/4913638-micron-technology-the-widening-infrastructure-thesis?source=feed"
+        "url": "https://seekingalpha.com/article/4913519-nasdaq-quant-leaders-5-top-stocks-averaging-272-percent-forward-eps-growth?source=feed"
       },
       {
-        "title": "Rates Spark: Oil Losing Control",
+        "title": "Credo Technology: The $3,000-Per-GPU Memory Arbitrage Demands A Strong Buy (Rating Upgrade)",
         "source": "Most Popular",
         "summary": "",
-        "publishedAt": "2026-06-10T08:20:00.000Z",
+        "publishedAt": "2026-06-10T12:11:46.000Z",
         "tickers": [],
-        "url": "https://seekingalpha.com/article/4913635-rates-spark-oil-losing-control?source=feed"
+        "url": "https://seekingalpha.com/article/4913681-credo-technology-the-3000-per-gpu-memory-arbitrage-demands-a-strong-buy-rating-upgrade?source=feed"
       },
       {
-        "title": "GigaCloud Technology: Cheap, Profitable, And Still Underestimated",
+        "title": "The Three Biggest Risks To Your Retirement",
         "source": "Most Popular",
         "summary": "",
-        "publishedAt": "2026-06-10T05:48:06.000Z",
+        "publishedAt": "2026-06-10T11:35:00.000Z",
         "tickers": [],
-        "url": "https://seekingalpha.com/article/4913586-gigacloud-technology-cheap-profitable-and-still-underestimated?source=feed"
+        "url": "https://seekingalpha.com/article/4913465-the-three-biggest-risks-to-your-retirement?source=feed"
       },
       {
-        "title": "Incyte: I Am Moving From Hold To Buy",
+        "title": "Micron: Just One Announcement Away",
         "source": "Most Popular",
         "summary": "",
-        "publishedAt": "2026-06-10T05:01:14.000Z",
+        "publishedAt": "2026-06-10T10:10:00.000Z",
         "tickers": [],
-        "url": "https://seekingalpha.com/article/4913582-incyte-i-am-moving-from-hold-to-buy?source=feed"
+        "url": "https://seekingalpha.com/article/4913608-micron-just-one-announcement-away?source=feed"
       },
       {
-        "title": "Micron: Dancing Near The Exit (Downgrade)",
+        "title": "The Stock Market's Breaking Point May Be Near",
         "source": "Most Popular",
         "summary": "",
-        "publishedAt": "2026-06-09T15:39:09.000Z",
+        "publishedAt": "2026-06-09T16:08:20.000Z",
         "tickers": [],
-        "url": "https://seekingalpha.com/article/4913324-micron-dancing-near-the-exit-downgrade?source=feed"
+        "url": "https://seekingalpha.com/article/4913339-the-stock-markets-breaking-point-may-be-near?source=feed"
+      },
+      {
+        "title": "Buy The Dip: Big Dividends Getting Cheap",
+        "source": "Most Popular",
+        "summary": "",
+        "publishedAt": "2026-06-09T15:05:00.000Z",
+        "tickers": [],
+        "url": "https://seekingalpha.com/article/4913155-buy-the-dip-big-dividends-getting-cheap?source=feed"
       },
       {
         "title": "Prepare For AI Volatility: 3 Dividend Stocks Powering The Grid",
@@ -247,14 +255,6 @@ window.redditHotStocksData = {
         "publishedAt": "2026-06-09T12:14:59.000Z",
         "tickers": [],
         "url": "https://seekingalpha.com/article/4913218-the-stock-market-crash-still-in-early-phase?source=feed"
-      },
-      {
-        "title": "If I Retired Tomorrow, This Is Exactly What I Would Do",
-        "source": "Most Popular",
-        "summary": "",
-        "publishedAt": "2026-06-09T11:30:00.000Z",
-        "tickers": [],
-        "url": "https://seekingalpha.com/article/4913044-if-i-retired-tomorrow-this-is-exactly-what-i-would-do?source=feed"
       },
       {
         "title": "A Golden Buying Opportunity On The Drop",
